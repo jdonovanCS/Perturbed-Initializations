649c10ef4f2184828678539640a88d06f5348e45
Jordan Donovan <jordan.t.donovancs@gmail.com>
2026-04-13 14:15:55 -0500

    added check to make sure I'm not creating runs in wandb that are empty at the end of runs

--- train_and_eval.py
+++ train_and_eval.py
@@ -152,7 +152,10 @@ def run():
         if inner_interval == 0:
             inner_interval = 1
         print("inner interval", inner_interval, "inner_skip", inner_skip, "inner_stop_after", inner_stop_after)
-        for n in range(int(inner_skip*inner_interval)-1, min(int(inner_skip*inner_interval*inner_stop_after), len(stored_filters[run_num])), max (1, inner_interval)):
+        loop_range_start = int(inner_skip*inner_interval)-1
+        loop_range_end = min(int(inner_skip*inner_interval*inner_stop_after), len(stored_filters[run_num])) 
+        loop_range_interval = max (1, inner_interval)
+        for n in range(loop_range_start, loop_range_end, loop_range_interval):
             if n < 0:
                 continue
             # i = n-1
@@ -173,6 +176,8 @@ def run():
             save_path = "trained_models/trained/conv{}_e{}_n{}_r{}_g{}.pth".format(not fixed_conv, experiment_name, name, run_num, n)
             print('Training and Evaluating: {} Gen: {} Run: {}'.format(name, n, run_num))
             record_progress = helper.train_network(data_module=data_module, filters=stored_filters[run_num][n], epochs=epochs, lr=args.lr, save_path=save_path, fixed_conv=fixed_conv, novelty_interval=int(args.novelty_interval), val_interval=int(args.test_accuracy_interval), diversity=diversity, scaled=scaled, devices=args.devices, save_interval=args.save_interval, bn=not args.no_bn, log_activations=args.log_activations)
+            if n+1*loop_range_interval >= loop_range_end:
+                continue
             helper.run(seed=False, rank=args.local_rank if args.local_rank > 0 else 0)
             helper.config['bn'] = not args.no_bn
             helper.config['dataset'] = args.dataset.lower()
